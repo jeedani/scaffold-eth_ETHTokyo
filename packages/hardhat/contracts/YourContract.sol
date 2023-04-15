abc735be19aa0a8758f17e426a6971fba0db2dcb
Daniel Jee <dan@Daniel-MacBookAir.local>
2023-04-15 16:25:15 +0900

    first mvp

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -2,25 +2,33 @@ pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol"; 
+import "@openzeppelin/contracts/access/Ownable.sol"; 
 // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
-contract YourContract {
+contract YourContract is Ownable {
 
   event SetPurpose(address sender, string purpose);
 
   string public purpose = "Building Unstoppable Apps!!!";
+  uint public price = 0.01 ether;
 
   constructor() payable {
     // what should we do on deploy?
   }
 
   function setPurpose(string memory newPurpose) public payable {
+      require(msg.value >= price, "Not enough price");
       purpose = newPurpose;
       console.log(msg.sender,"set purpose to",purpose);
       emit SetPurpose(msg.sender, purpose);
+      price = price * 105 / 100;
   }
 
+  function withdraw() public onlyOwner {
+      (bool success, ) = msg.sender.call{value: address(this).balance}("");
+  }
+
+  
   // to support receiving ETH by default
   receive() external payable {}
   fallback() external payable {}
